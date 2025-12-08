6f4dd7987d81e566e38b67511d879f462ee3b876
Kasunisarangi <kasuni.aventure@gmail.com>
2025-12-08 09:18:12 +0530

    next update

--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Image } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
 
@@ -17,12 +17,13 @@ const HeroExperience = () => {
       <ambientLight intensity={0.2} color="#1a1a40" />
       {/* Configure OrbitControls to disable panning and control zoom based on device type */}
       <OrbitControls
-        enablePan={false} // Prevents panning of the scene
-        enableZoom={!isTablet} // Disables zoom on tablets
-        maxDistance={20} // Maximum distance for zooming out
-        minDistance={5} // Minimum distance for zooming in
-        minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
-        maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
+        enablePan={false}
+        // enableZoom={!isTablet}
+        enableZoom={false}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
       />
 
       <Suspense fallback={null}>
@@ -33,8 +34,10 @@ const HeroExperience = () => {
           position={[0, -3.5, 0]}
           rotation={[0, -Math.PI / 4, 0]}
         >
-          <Room />
+          {/* <Room /> */}
+          {/* <Image url="/images/rsar.png" scale={[9, 9]} transparent /> */}
         </group>
+        <Image url="/images/redpic.png" scale={[7, 10]} transparent />
       </Suspense>
     </Canvas>
   );
